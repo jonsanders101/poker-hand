adc0573ad5c604305626e44f1aa6c94fc39d89bd
Jon Sanders <jonsanders101@aol.com>
2018-06-17 18:56:34 +0100

    Recognise four of a kind as higher than three of a kind

--- spec/unit/indexSpec.js
+++ spec/unit/indexSpec.js
@@ -1,7 +1,7 @@
 import compareHands from '../../lib/poker';
 
 describe('compareHands', () => {
-    describe('Given that Black as a straight flush and White has a Four of a Kind', () => {
+    describe('Given that Black has a straight flush and White has a Four of a Kind', () => {
         it('Should report that black has won', () => {
             const inputString = 'Black: AH KH QH JH 10H White: AH AC AD AS 10H';
             expect(compareHands(inputString)).toEqual('Black wins. - with straight flush');
@@ -17,6 +17,12 @@ describe('compareHands', () => {
         it('Should report thatblack has won', () => {
             const inputString = 'Black: AH KH QH JH 10H White: AC KC QC JC 10C';
             expect(compareHands(inputString)).toEqual('Black wins. - with straight flush');
+        });
+    });
+    describe('Given that both Black has a four of a kind and White has a three of a kind', () => {
+        it('Should report that black has won', () => {
+            const inputString = 'Black: AH AC AD AS 10H White: KH KC KD QS JH';
+            expect(compareHands(inputString)).toEqual('Black wins. - with four of a kind');
         });
     });
 });
